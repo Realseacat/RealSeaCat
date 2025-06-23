47a19407ef3e461b0b306b54108763b9168200a7
Realseacat <vvn3353@gmail.com>
2025-06-23 09:36:18 +0700

    Update SeaCatHub.lua

--- SeaCatHub.lua
+++ SeaCatHub.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 function loadBloxFruitsHub()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/BloxFuit/refs/heads/main/SeaCattHub.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/BloxFuit/refs/heads/main/SeaCathub.lua"))()
 end
 
 if table.find({2753915549, 4442272183, 7449423635}, game.PlaceId) then
